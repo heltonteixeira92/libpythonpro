1cff469a7ec050d4c361c9858509d0e3f2274e6c
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2020-11-24 20:11:55 -0300

    Adicionada flake8 como dependência

--- README.md
+++ README.md
@@ -5,6 +5,19 @@ Nesse curso é ensinado como contribuir com projetos de código aberto.
 
 Link do curso [Python pro](https://www.python.pro.br)
 
+Para Instalar:
+
+'''console
+python -m venv .venv
+.venv\Scripts\activate
+pip install -r requirements-dev.txt
+'''
+
+Para conferir qualidade de código:
+''' 
+flake8
+'''
+
 Tópicos a serem abordados:
 1. Git
 2. Virtualenv
--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def buscar_avatar(usuario):
     """
     Busca o avatar do usuário no Github
@@ -8,7 +9,8 @@ def buscar_avatar(usuario):
     """
     url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
-    return resp.json() ['avatar_url']
+    return resp.json()['avatar_url']
+
 
 if __name__ == "__main__":
     print(buscar_avatar('heltonteixeira92'))
