c3878824107a36552c7740bfbb8d5a36ecc59497
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2020-11-28 12:09:33 -0300

    Inserido test com pytest

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ def find_package_data(
     return out
 
 
-PACKAGE = "libpythonpro"
+PACKAGE = "libpythonprobyhelton"
 NAME = PACKAGE
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "Helton Teixeira de Souza"
