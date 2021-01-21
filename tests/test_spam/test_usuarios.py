bf1393544aed92b6e0ff3c8bb50314856d113c4e
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-01-21 13:45:21 -0300

    BD Tests

---
+++ tests/test_spam/test_usuarios.py
@@ -0,0 +1,36 @@
+import pytest
+
+from spam.db import Conexao
+from spam.modelos import Usuario
+
+
+@pytest.fixture   # decorator chamado fixture
+def conexao():
+    #Setup
+    conexao_obj = Conexao()
+    yield conexao_obj
+    # Tear Down
+    conexao_obj.fechar()
+
+@pytest.fixture
+def sessao(conexao):
+    sessao_obj = conexao.gerar_sessao()
+    yield sessao_obj
+    sessao_obj.roll_back()
+    sessao_obj.fechar()
+
+
+def test_salvar_usuario(sessao):
+    usuario = Usuario(nome='Renzo')
+    sessao.salvar(usuario)
+    assert isinstance(usuario.id, int)
+
+
+
+
+
+def test_listar_usuarios(sessao):
+    usuarios = [Usuario(nome='Renzo'), Usuario(nome='Luciano')]
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    assert usuarios == sessao.listar()
